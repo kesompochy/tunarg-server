62862a9bd2db9f0cfc87dbdf297a738eaaa66188
kesompochy <kesompochy@gmail.com>
2022-03-23 11:24:26 +0900

    hogehoge

--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Server from './server';
-import Protocol from './protocol';
-export { Server, Protocol };
-//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import Server from './server';
-import Protocol from './protocol';
-export {Server, Protocol};
